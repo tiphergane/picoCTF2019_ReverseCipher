712664cb1dce79d5b3aa1a6e13565456e76ed460
tiphergane <tiphergane@gmail.com>
2020-01-07 18:13:27 +0100

    Ajoutez des fichiers projet.

---
+++ ReverseCipher/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+
+namespace ReverseCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] BaseFlag = new byte[32];
+            BaseFlag = Encoding.UTF8.GetBytes("picoCTF{w1{1wq80haib767}");
+            int i;
+            for (i = 8; i < BaseFlag.Length - 1; i++)
+            {
+                int value = i % 2; //check si i est pair ou impair
+                if (value == 0) // si pair alors
+                {
+                   BaseFlag[i] -= 5;
+                }
+                else
+                    BaseFlag[i] += 2;
+            };
+            string f = Encoding.UTF8.GetString(BaseFlag, 0, BaseFlag.Length);
+            Console.WriteLine("La clef est : {0}", f);
+            Console.WriteLine("Press Enter to quit");
+            while (Console.ReadKey().Key != ConsoleKey.Enter) ;
+        }
+    }
+}
